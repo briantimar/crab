7dd47b953c8ce87369b5ff94116575ecb19269d8
Brian Timar <timarbrian@gmail.com>
2018-05-17 02:36:43 -0700

    first commit

---
+++ optimize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 17 02:28:47 2018
+
+@author: btimar
+"""
+
+import numpy as np
+
+from scipy.optimize import minimize
+
+def do_nm_minimize
+
+
+
+
+
+#f = lambda x: np.sum(x**4)
+#x0 =np.array([1,1,2,4])
+#res = minimize(f, x0, method='Nelder-Mead',tol=1E-9)
+#
